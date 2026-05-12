bb01317294ad890e69bf6f615320c6910685365c
Gia Han <giahanphm126@gmail.com>
2026-05-13 02:08:48 +1000

    add endpoints for records router

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from core.database import create_db_and_tables
 import models
+from routes import records as records_router
 
 app = FastAPI()
+
+app.include_router(records_router.router)
 
 @app.on_event("startup")
 def on_startup():
---
+++ backend/routes/records.py
@@ -0,0 +1,55 @@
+from typing import List
+from fastapi import APIRouter, HTTPException, Depends, Response, status
+from sqlmodel import Session, select
+from core.database import get_session
+from models import Record
+from schema import RecordCreate, RecordRead, RecordUpdate
+
+router = APIRouter(prefix="/records", tags=["records"])
+
+#endpoint to create a new record
+@router.post("/", response_model=RecordRead, status_code=status.HTTP_201_CREATED)
+def create_record(record_create: RecordCreate, session: Session = Depends(get_session)):
+    record = Record.model_validate(record_create)
+    session.add(record)
+    session.commit()
+    session.refresh(record)
+    return record
+
+#endpoint to get all records
+@router.get("/", response_model=List[RecordRead])
+def get_all_records(session: Session = Depends(get_session)):
+    records = session.exec(select(Record)).all()
+    return records
+
+#endpoint to get a record by id
+@router.get("/{record_id}", response_model=RecordRead)
+def get_record(record_id : int, session: Session = Depends(get_session)):
+    record = session.get(Record, record_id)
+    if not record:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Record not found")
+    return record
+
+#endpoint to update a record by id
+@router.patch("/{record_id}", response_model = RecordRead)
+def update_record(record_id: int, record_update: RecordUpdate, session: Session = Depends(get_session)):
+    record = session.get(Record, record_id)
+    if not record:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Record not found")
+    record_data = record_update.model_dump(exclude_unset=True)
+    for key, value in record_data.items():
+        setattr(record, key, value)
+    session.add(record)
+    session.commit()
+    session.refresh(record)
+    return record
+
+#endpoint to delete a record by id
+@router.delete("/{record_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_record(record_id: int, session: Session = Depends(get_session)):
+    record = session.get(Record, record_id)
+    if not record:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Record not found")
+    session.delete(record)
+    session.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
